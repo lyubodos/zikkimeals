3c71ab7c035f5cc7d688ff781aebdeb44d20dbfa
Vasilev <Lyubomir.Vasilev@adesso.bg>
2022-01-17 15:12:26 +0100

    MealItemForm.js child component created and set to track the quantity of the ordered meals from the user.
    Change was needed for better UX.#

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Input from '../../UI/Input/Input';
 
-export default function MealItemForm() {
+import classes from "./MealItemForm.module.css";
+
+export default function MealItemForm(props) {
     return (
-        <div>
-            
-        </div>
+        <form className={classes.form}>
+            <Input label="Amount" input={{
+                id: props.id,
+                type: "number",
+                min: '1',
+                max: '5',
+                step: '1',
+                defaultValue: "1"
+            }} />
+            <button>+ Add</button>
+        </form>
     )
 }
