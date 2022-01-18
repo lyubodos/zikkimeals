9ed53faa9de9fe1e8c85f4824a47be238efc60b3
Vasilev <Lyubomir.Vasilev@adesso.bg>
2022-01-18 18:58:00 +0100

    Cart functionality optimize
    Cart functionality has been optimized. Users can now choose to increase
    or decrease the amount of the items in their cart. Front-end has been
    finished. Similar items should are still shown separatly.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,37 +1,52 @@
 import React from "react";
+import { useContext } from "react/cjs/react.development";
+import CartContext from "../../store/cart-conext";
 import Modal from "../UI/Modal/Modal";
 
 import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
 
 export default function Cart(props) {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `${cartCtx.totalAmount.toFixed(2)}$`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHanlder = (id) => {};
+
+  const cartAddHanlder = (item) => {};
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[
-        {
-          id: "c1",
-          name: "Pasta",
-          description: "A delicious spagetti carbonara from one of the best chefs in Italy",
-          amount: 2,
-          price: 11.99,
-        },
-      ].map((cart) => (
-        <li key={cart.id}>{cart.name}</li>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          {...item}
+          onRemove={cartItemRemoveHanlder.bind(null, item.id)}
+          onAdd={cartAddHanlder.bind(null, item)}
+        >
+          {}
+        </CartItem>
       ))}
     </ul>
   );
 
   return (
     <Modal onHideCart={props.onHideCart}>
-        {cartItems}
-        <div>
-          <p>A delicious spagetti carbonara from one of the best chefs in Italy</p>
-          <span>Total Amonut: </span>
-          <span>35.12$</span>
-        </div>
-        <div className={classes.actions}>
-          <button className={classes["button--alt"]} onClick={props.onHideCart}>Close</button>
-          <button className={classes.button}>Order</button>
-        </div>
+      {cartItems}
+      <div>
+        <p>
+          A delicious spagetti carbonara from one of the best chefs in Italy
+        </p>
+        <span>Total Amonut: </span>
+        <span>35.12$</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]} onClick={props.onHideCart}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
+      </div>
     </Modal>
   );
 }
