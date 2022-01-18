516229cb84428fcb7784edd64eae6f314ad6583f
Vasilev <Lyubomir.Vasilev@adesso.bg>
2022-01-18 17:59:45 +0100

    Change CartProvider Context
    CartProvider context was optimized with the help of the useReducer hook.
    Now the total amount of all of the added items in the cart is being
    tracked.
    
    Change was required for better UX.

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import CartContext from './cart-conext';
+import React, { useReducer } from "react";
+import CartContext from "./cart-conext";
 
 
+const defaultState = {
+    items: [],
+    totalAmount: 0
+};
+
+const ACTIONS = {
+    ADD_ITEM : 'ADD_ITEM',
+    RMV_ITEM : 'REMOVE_ITEM'
+}
+
+const cartReducer = (state, action) => {
+    if(action.type === 'ADD_ITEM'){
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + action.item.price *action.item.amount;
+
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        };
+    };
+
+
+    return defaultState;
+}
 
 export default function CartProvider(props) {
 
-    const addItemToCartHandler = item => {};
+  const [cartState, dispatchCart] = useReducer(cartReducer, defaultState)
 
-    const removeItemFromHandler = id => {};
+  const addItemToCartHandler = (item) => {
+      dispatchCart({type: 'ADD_ITEM', item: item});
+  };
 
-    const cartContext = {
-            items: [],
-            totalAmount: 0,
-            addItem: addItemToCartHandler,
-            removeItem: removeItemFromHandler
-    };
+  const removeItemFromHandler = (id) => {
+    dispatchCart({type: 'REMOVE_ITEM', id: id});
+  };
 
-    return (
-        <CartContext.Provider value={cartContext}>
-            {props.children}
-        </CartContext.Provider>
-    )
+
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromHandler,
+  };    
+
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
 }
