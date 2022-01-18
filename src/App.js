ba227210d991294295868036cb0000746cfb5746
Vasilev <Lyubomir.Vasilev@adesso.bg>
2022-01-18 17:45:22 +0100

    Add quantity counter in CartButton
    A quantity counter function has been added to the HeaderCartButton
    component, which allows the user to track how many items are currently
    added in his/her's cart.
    
    Implementation was required for better UX.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import CartProvider from "../store/CartProvider";
+import CartProvider from "./store/CartProvider";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header/Header";
 import Meals from "./components/Meals/Meals";
--- src/components/Layout/Header/HeaderCartButton.js
+++ src/components/Layout/Header/HeaderCartButton.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, {useContext} from "react";
+import CartContext from "../../../store/cart-conext";
 import CartIcon from "../../Cart/CartIcon";
 
 import classes from "./HeaderCartButton.module.css";
 
 export default function HeaderCartButton(props) {
+
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((currNum, item) => {
+    return currNum + item.amount;
+  }, 0)
+
   return (
     <button className={classes.button} onClick={props.onShowCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span >Your Cart</span>
-      <span className={classes.badge}>2</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 }
