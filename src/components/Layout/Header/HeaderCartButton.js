ec356eba90461a19e9ec1645e8174add6b50ac26
Vasilev <Lyubomir.Vasilev@adesso.bg>
2022-01-19 14:58:29 +0100

    Add a visual effect for adding items in Cart
    Added a visual effect for the cart button in the HeaderButton component.
    With the help of the useEffect hoook, we are now able to visually
    display the change of the quantiti of items in the user's cart.
    
    Implmentation was required for a better UX/UI.

--- src/components/Layout/Header/HeaderCartButton.js
+++ src/components/Layout/Header/HeaderCartButton.js
@@ -1,19 +1,39 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import CartContext from "../../../store/cart-conext";
 import CartIcon from "../../Cart/CartIcon";
 
 import classes from "./HeaderCartButton.module.css";
 
 export default function HeaderCartButton(props) {
-
   const cartCtx = useContext(CartContext);
+  const [btnHighlight, setBtnHighlight] = useState(false);
 
-  const numberOfCartItems = cartCtx.items.reduce((currNum, item) => {
+  const {items} = cartCtx;
+
+  const numberOfCartItems = items.reduce((currNum, item) => {
     return currNum + item.amount;
-  }, 0)
+  }, 0);
+
+  const brnClasses = `${classes.button} ${btnHighlight ? classes.bump : ""}`
+
+  useEffect(() => {
+    if(items.length === 0){
+      return;
+    }
+    setBtnHighlight(true);
+    
+    const timer = setTimeout(() => {
+      setBtnHighlight(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+
+  }, [items])
 
   return (
-    <button className={classes.button} onClick={props.onShowCart}>
+    <button className={brnClasses} onClick={props.onShowCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
