8fb90ef6eb559bb100755c2a6ab41f704d8139b3
Vasilev <Lyubomir.Vasilev@adesso.bg>
2022-02-10 17:57:10 +0100

    Add loading and error handling
    Added a loading option for informing the user that his/hers data is
    being processed. Error message was also added for entering an incorrect
    url for fething data.

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -4,43 +4,78 @@ import Card from "../UI/Card/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 
-
 export default function AvailableMeals() {
-
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState("");
 
   useEffect(() => {
-
     const reqData = async () => {
-      const resData = await fetch('https://zikki-meals-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
+      const resData = await fetch(
+        "https://zikki-meals-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+      );
+
+      if(!resData.ok){
+        throw new Error("Something went wrong!");
+      }
+
       const data = await resData.json();
 
       const loaddedData = [];
 
       for (const keyObj in data) {
-       loaddedData.push({
+        loaddedData.push({
           key: keyObj,
           name: data[keyObj].name,
           description: data[keyObj].description,
-          price: data[keyObj].price
-        })
+          price: data[keyObj].price,
+        });
       }
       setMeals(loaddedData);
-    }
+    };
 
-    reqData();
-      
-  }, [])
+      reqData()
+      .then()
+      .catch(err => {
+        setHttpError(err.message)
+        setIsLoading(false);
+      });
 
-  const storedMeals = meals.map((meal) => {
-    return <MealItem {...meal} key={meal.id}>{meal.name}</MealItem>;
+      setIsLoading(false);
+
+
+  }, []);
+
+
+  if (isLoading) {
+    return (
+      <section className={classes.mealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if(httpError){
+    return(
+      <section className={classes.mealsError}>
+      <p>{httpError}</p>
+    </section>
+    );
+  }
+
+  const storedMeals = meals.map( meal => {
+    return (
+      <MealItem {...meal} key={meal.id}>
+        {meal.name}
+      </MealItem>
+    );
   });
 
   return (
     <section className={classes.meals}>
       <Card>
-      <ul>{storedMeals}</ul>
+        <ul>{storedMeals}</ul>
       </Card>
     </section>
   );
-}
+};
