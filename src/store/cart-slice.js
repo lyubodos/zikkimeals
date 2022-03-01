0a0e9fca9fd0c337471cf1030aec065b9540ed0a
Vasilev <Lyubomir.Vasilev@adesso.bg>
2022-03-01 16:52:38 +0100

    CartSlice file create
    CartSlice js file was created and a basic logic was implemented for the
    cart's state. Two functions were provided for adding and removeing items
    in the cart.
    
    Implementation is required for basic redux functionality.

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -4,7 +4,7 @@ import {createSlice} from "@reduxjs/toolkit";
 const cartInitialState = {
     items: [],
     changed: false,
-    totalQuantiti: 0
+    totalQuantity: 0
 }
 
 createSlice({
@@ -12,10 +12,43 @@ createSlice({
     initialState: cartInitialState,
     reducers: {
         addItemToCart(state, action) {
+            state.totalQuantity++;
+            state.changed = true;
 
+            const newItem = action.payload;
+
+            const existingItem = items.find(item => item.id === newItem.id );
+
+            if(existingItem){
+                existingItem.quantity++;
+                existingItem.totalPrice += existingItem.price;
+
+            } else {
+                state.items.push({
+                    id: existingItem.id,
+                    name: existingItem.name,
+                    description: existingItem.description,
+                    price: existingItem.price,
+                    quantity: 1,
+                    totalPrice: existingItem.price
+                })
+            }
         },
         removeItemFromCart(state, action){
+            state.changed = true;
+            state.totalQuantity--;
+            
+            const id = action.payload;
+            const existingItem = state.items.find((item) => item.id === id);
 
+    
+            if(existingItem.quantity === 1) {
+                state.items.filter(item => item.id !=== id)
+            } 
+
+            existingItem.quantity--;
+            existingItem.totalPrice -= existingItem.price;
+            
         }
     }
 })
